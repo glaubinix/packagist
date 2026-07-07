2b87499a85c62b74f2ffd483f5aa95f579aa33c6
Igor Benko <i.benko@packagist.com>
2026-07-07 17:48:40 +0200

    Cleanup after rebase / fix tests

--- src/Controller/OrganizationController.php
+++ src/Controller/OrganizationController.php
@@ -298,10 +298,6 @@ class OrganizationController extends Controller
     #[Route(path: '/organizations/{organization}/teams/{team}/members/{teamMember}/remove', name: 'organization_team_member_remove', methods: ['GET', 'POST'], requirements: ['organization' => Slug::PATTERN, 'team' => Requirement::ULID])]
     public function removeTeamMember(Request $request, Organization $organization, OrganizationTeam $team, User $teamMember, #[CurrentUser] User $user): Response
     {
-        if ($redirect = $this->require2fa($user)) {
-            return $redirect;
-        }
-
         $form = $this->createForm(RemoveTeamMemberType::class);
         $form->handleRequest($request);
 
@@ -363,10 +359,6 @@ class OrganizationController extends Controller
     #[Route(path: '/organizations/{organization}/members/{organizationMember}/remove', name: 'organization_member_remove', methods: ['GET', 'POST'], requirements: ['organization' => Slug::PATTERN])]
     public function removeMember(Request $request, Organization $organization, User $organizationMember, #[CurrentUser] User $user): Response
     {
-        if ($redirect = $this->require2fa($user)) {
-            return $redirect;
-        }
-
         $form = $this->createForm(RemoveMemberType::class);
         $form->handleRequest($request);
 
